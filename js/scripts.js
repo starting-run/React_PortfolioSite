dfb4beb95b9a3ae931dd83d223c220d9fff50305
usfree <tlrmsjtm77@naver.com>
2023-10-20 22:32:02 +0900

    asdasd

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,34 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+    /* Back to Top
+    * ------------------------------------------------------ */
+
+(function(html) {
+    const ssBackToTop = function() {
+
+        const pxShow = 900;
+        const goTopButton = document.querySelector(".ss-go-top");
+
+        if (!goTopButton) return;
+
+        // Show or hide the button
+        if (window.scrollY >= pxShow) goTopButton.classList.add("link-is-visible");
+
+        window.addEventListener('scroll', function() {
+            if (window.scrollY >= pxShow) {
+                if(!goTopButton.classList.contains('link-is-visible')) goTopButton.classList.add("link-is-visible")
+            } else {
+                goTopButton.classList.remove("link-is-visible")
+            }
+        });
+
+    }; // end ssBackToTop
+
+    (function ssInit() {
+
+        ssBackToTop();
+
+    })();
+})(document.documentElement);
