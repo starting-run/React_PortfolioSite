2402ad3acee075a1de7d642fe0861766fccaf906
usfree <tlrmsjtm77@naver.com>
2023-10-23 21:18:10 +0900

    dd

--- js/scripts.js
+++ js/scripts.js
@@ -30,12 +30,6 @@
 
     }; // end ssBackToTop
 
-    (function ssInit() {
-
-        ssBackToTop();
-
-    })();
-
     document.addEventListener('DOMContentLoaded', function(){
         //다크모드 토글
         if(document.querySelector('.darkmode')){
@@ -59,5 +53,13 @@
         }
 
     })
+    
+    (function ssInit() {
+
+        ssBackToTop();
+
+    })();
+
+
     document.oncontextmenu = function(){return false;}
 })(document.documentElement);
