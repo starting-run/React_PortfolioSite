0473625cb7837d54991f3e4aae520199b73ab9c3
usfree <tlrmsjtm77@naver.com>
2023-10-27 00:06:49 +0900

    dd

--- js/scripts.js
+++ js/scripts.js
@@ -59,5 +59,17 @@
         }
 
     })
+    jQuery(function($) {
+        $("body").css("display", "none");
+        $("body").fadeIn(2000);
+        $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage);
+        });
+        function redirectPage() {
+        window.location = linkLocation;
+        }
+        });
     document.oncontextmenu = function(){return false;}
 })(document.documentElement);
