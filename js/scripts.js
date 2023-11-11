bc6cdee9f0d6e520970cdc1b3dda6046e0c24eb3
usfree <tlrmsjtm77@naver.com>
2023-11-11 23:06:06 +0900

    dd

--- js/scripts.js
+++ js/scripts.js
@@ -73,4 +73,19 @@
         }
         });
     document.oncontextmenu = function(){return false;}*/
+
+    const modalOpenButton = document.getElementById('modalOpenButton');
+    const modalCloseButton = document.getElementById('modalCloseButton');
+    const modal = document.getElementById('modalContainer');
+
+    modalOpenButton.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    });
+
+    modalCloseButton.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    });
+
 })(document.documentElement);
+
+
