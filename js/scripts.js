3b9b8c58578f8e7e26115b367503b951792a5c83
usfree <tlrmsjtm77@naver.com>
2023-11-19 01:25:24 +0900

    dd

--- js/scripts.js
+++ js/scripts.js
@@ -35,15 +35,15 @@
         ssBackToTop();
 
     })();
-
+/*
     document.addEventListener('DOMContentLoaded', function(){
 
         function checkDarkMode() {
-            return window.matchMedia('(prefers-color-scheme: dark)').matches;
+            return window.matchMedia('(prefers-color-scheme: light)').matches;
         }
         //다크모드 토글
         if(document.querySelector('.darkmode')){
-            if (checkDarkMode()) {
+            if(localStorage.getItem("darkmode") == 'on'){
                 //다크모드 켜기
                 document.body.dataset.darkmode='on';
                 document.querySelector('#toggle-radio-dark').checked = true;
@@ -63,7 +63,7 @@
             localStorage.removeItem("darkmode");
         }
 
-    })
+    })*/
     /*
     jQuery(function($) {
         $("body").css("display", "none");
