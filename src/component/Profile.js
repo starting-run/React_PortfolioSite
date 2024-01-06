d8cabf6991a5eb8e6fe329e1ceb70fb7bece1b23
usfree <tlrmsjtm77@naver.com>
2024-01-07 01:01:29 +0900

    profile

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -30,44 +30,40 @@ class Profile extends Component {
                             </div>
                             <div class="col-auto px-5">
                                 <div class="mt-2"><div class="btn btn-primary-round-outline pt-0"><span class="fs-7 font-3">USFREE</span></div></div>
-                                <div class="mb-0"><span class="fs-2 font-3 fw-bolder">조우현</span></div>
-                                <div class="font-3 fs-7 fw-bold">Woohyun Cho</div>
+                                <div class="mb-0"><span class="fs-2 font-3 fw-bolder text-black">조우현</span></div>
+                                <div class="font-3 fs-7 fw-bold text-black">Woohyun Cho</div>
                             </div>
                         </div>
                         <div class="row row-cols-2 row-cols-lg-5  justify-content-center font-3 mb-5">
                             <div class="col p-2">
                                 <div class="border-0 rounded-2 card-blur-info">
                                     <div class="p-3">
-                                        <span class="font-3 fs-6 text-main mb-5"><FontAwesomeIcon icon={faCakeCandles}/></span>
-                                        <div class="mt-3 fs-3 fw-bolder no_enter text-infopage"><span class="union_level"></span></div>
-                                        <div class="fs-7 fw-light2 text-infopage">1999. 07. 23.</div>
+                                        <span class="font-3 fs-6 text-main mb-5"><FontAwesomeIcon icon={faCakeCandles}/>　<span class="fs-7 fw-light2 text-infopage">1999. 07. 23.</span></span>
+                                        
                                     </div>
                                 </div>
                             </div>
                             <div class="col p-2">
                                 <div class="border-0 rounded-2 card-blur-info">
                                     <div class="p-3">
-                                        <span class="font-3 fs-6 text-main mb-5"><FontAwesomeIcon icon={faEnvelopeOpenText}/></span>
-                                        <div class="mt-3 fs-3 fw-bolder text-infopage"><span class="dojang_best_floor"></span></div>
-                                        <div class="fs-7 fw-light2 text-infopage"><a href="mailto:ploris576@kangnam.ac.kr?subject=usfree.site에서 보내는 메일입니다.">ploris576@kangnam.ac.kr</a></div>
+                                        <span class="font-3 fs-6 text-main mb-5"><FontAwesomeIcon icon={faLocationDot}/>　<span class="fs-7 fw-light2 text-infopage">Mapo-gu, Seoul</span></span>
+                                        
                                     </div>
                                 </div>
                             </div>
                             <div class="col p-2">
                                 <div class="border-0 rounded-2 card-blur-info">
                                     <div class="p-3">
-                                        <span class="font-3 fs-6 text-main mb-5"><FontAwesomeIcon icon={faLocationDot}/></span>
-                                        <div class="mt-3 fs-3 fw-bolder text-infopage"><span class="theseed_floor"></span></div>
-                                        <div class="fs-7 fw-light2 text-infopage">Mapo-gu, Seoul</div>
+                                        <span class="font-3 fs-6 text-main mb-5 no_enter"><FontAwesomeIcon icon={faEnvelopeOpenText}/>　<span class="fs-7 fw-light2 text-infopage"><a href="mailto:ploris576@kangnam.ac.kr?subject=usfree.site에서 보내는 메일입니다.">ploris576@kangnam.ac.kr</a></span></span>
+                                        
                                     </div>
                                 </div>
                             </div>
                             <div class="col p-2">
                                 <div class="border-0 rounded-2 card-blur-info">
                                     <div class="p-3">
-                                        <span class="font-3 fs-6 text-main mb-5"><FontAwesomeIcon icon={faChartSimple}/></span>
-                                        <div class="mt-3 fs-3 fw-bolder text-infopage"><span class="trophy_score"></span></div>
-                                        <div class="fs-7 fw-light2 text-infopage"><a href="https://solved.ac/profile/tlrmsjtm77">BOJ / tlrmsjtm77</a></div>
+                                        <span class="font-3 fs-6 text-main mb-5"><FontAwesomeIcon icon={faChartSimple}/>　<span class="fs-7 fw-light2 text-infopage"><a href="https://solved.ac/profile/tlrmsjtm77">BOJ / tlrmsjtm77</a></span></span>
+                                        
                                     </div>
                                 </div>
                             </div>
