fa2b8c19669ea7e4a7d6de892568a5e070b5f20e
Woohyun Cho <tlrmsjtm77@naver.com>
2024-03-31 06:28:42 +0900

    메인 추가수정 2

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,7 +4,7 @@ import { HashLink } from 'react-router-hash-link';
 import { Helmet } from 'react-helmet';
 import { Fade } from 'react-reveal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faCircleArrowDown, faCircleArrowRight, faCircleArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
     const [text, setText] = useState('UNREAL DEV.');
@@ -34,6 +34,7 @@ function Home() {
     <div>
         <Helmet><title>USFREE | Woohyun Cho</title></Helmet>
         <div class="section bg-white-gray">
+        {/*
             <div class="w-100">
                 <div class="scr-down font-7 fw-light2">
                     <div class="main__action text-center">
@@ -48,15 +49,16 @@ function Home() {
                         </HashLink>
                     </div>
                 </div>
-            </div>
+            </div>*/}
+
             <div class="s-intro pt-3">
                 <div class="s-intro__content row text-black pt-5"> 
                     <div class="justify-content-center font-3">
                         <div class="container-xxl px-3 mt-5">
                             <div class="row row-cols-lg-2 row-cols-1">
-                                <div class="col col-lg-9 mb-3 card p-0 rounded-4 card-border-0">
-                                    <div class="intro-background">
-                                        <div class="text-black fs-0 lh-sm font-11 mb-5 pt-5">
+                                <div class="col col-lg-9 mb-3 ">
+                                    <div class="intro-background card rounded-4 card-border-0 p-0">
+                                        <div class="text-black fs-0 lh-sm font-11 mb-5 pt-6">
                                             <div class="font-letter-space fw-bold2 pt-5 mt-5">
                                                 <span className={`fade-in-out ${fadeIn ? 'fade-in' : 'fade-out'} rainbow-text`}>{text}</span>
                                             </div>
@@ -67,6 +69,12 @@ function Home() {
                                                 <div class="fw-bold2 highlight">Developer Portfolio</div>
                                             </div>
                                         </div>
+                                        <span class="text-end fs-4 card-in-arrow">
+                                            <div class="fs-6 px-4 mx-2 mb-1-5">더보기</div>
+                                        </span>
+                                        <span class="text-end fs-4 card-in-arrow">
+                                            <HashLink smooth to="#intro" className="main__scroll"><FontAwesomeIcon icon={faCircleArrowDown}/></HashLink>
+                                        </span>
                                     </div>
                                 </div>
                                 <div class="col col-lg-3">
@@ -75,10 +83,10 @@ function Home() {
                                             <NavLink to='/profile'>
                                                 <div class="img-fluid mb-3">
                                                     <div class="col">
-                                                        <div class="card card-cover-profile h-100 overflow-hidden text-black card-border-0 rounded-4">
+                                                        <div class="card card-cover-profile h-100 overflow-hidden text-only-black card-border-0 rounded-4">
                                                             <div class="d-flex flex-column h-100 p-4 pb-3 card-overtext">
                                                                 <span class="mb-2 fs-6 lh-1 fw-normal font-11 text-start mb-6">프로필</span>
-                                                                <span class="text-start fw-bold fs-5 mb-3">안녕하세요! 개발자 조우현에 대해 소개합니다.</span>
+                                                                <span class="text-start fw-bold fs-5 mb-3">안녕하세요! 개발자 <span class="fw-bold2">조우현</span>의 프로필에 대해 소개합니다.</span>
                                                                 <span class="text-end fs-4 card-in-arrow">
                                                                     <FontAwesomeIcon icon={faCircleArrowRight}/>
                                                                 </span>
@@ -95,7 +103,7 @@ function Home() {
                                                         <div class="card card-cover-main h-100 overflow-hidden text-black card-border-0 rounded-4">
                                                             <div class="d-flex flex-column h-100 p-4 pb-3 card-overtext">
                                                                 <span class="mb-2 fs-6 lh-1 fw-normal font-11 text-start mb-6">프로젝트</span>
-                                                                <span class="text-start fw-bold fs-5 mb-3">지금까지 진행한 프로젝트와 상세내용을 확인할 수 있습니다.</span>
+                                                                <span class="text-start fw-bold fs-5 mb-3">그동안 진행한 프로젝트와 상세내용을 확인할 수 있습니다.</span>
                                                                 <span class="text-end fs-4 card-in-arrow">
                                                                     <FontAwesomeIcon icon={faCircleArrowRight}/>
                                                                 </span>
@@ -112,9 +120,41 @@ function Home() {
                                                         <div class="card card-cover-main h-100 overflow-hidden text-black card-border-0 rounded-4">
                                                             <div class="d-flex flex-column h-100 p-4 pb-3 card-overtext">
                                                                 <span class="mb-2 fs-6 lh-1 fw-normal font-11 text-start mb-6">깃허브</span>
-                                                                <span class="text-start fw-bold fs-5 mb-3">진행한 프로젝트와 공부한 내용 등 여러가지 리포지토리를 관리합니다.</span>
+                                                                <span class="text-start fw-bold fs-5 mb-3">진행한 프로젝트와 스터디 내용 등 여러가지 리포지토리를 관리합니다.</span>
                                                                 <span class="text-end fs-4 card-in-arrow">
-                                                                    <FontAwesomeIcon icon={faCircleArrowRight}/>
+                                                                    <FontAwesomeIcon icon={faCircleArrowUp} className='rot-45deg'/>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="col">
+                                            <a href="https://velog.io/@usfree" target='_blank'>
+                                                <div class="img-fluid mb-3">
+                                                    <div class="col">
+                                                        <div class="card card-cover-blog h-100 overflow-hidden text-only-white card-border-0 rounded-4">
+                                                            <div class="d-flex flex-column h-100 p-4 pb-3 card-overtext">
+                                                                <span class="fs-6 lh-1 fw-normal font-11 text-start mb-1">블로그</span>
+                                                                <span class="text-end fs-4 card-in-arrow">
+                                                                    <FontAwesomeIcon icon={faCircleArrowUp} className='rot-45deg'/>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="col">
+                                            <a href="https://velog.io/@usfree" target='_blank'>
+                                                <div class="img-fluid mb-3">
+                                                    <div class="col">
+                                                        <div class="card card-cover-boj h-100 overflow-hidden text-only-white card-border-0 rounded-4">
+                                                            <div class="d-flex flex-column h-100 p-4 pb-3 card-overtext">
+                                                                <span class="fs-6 lh-1 fw-normal font-11 text-start mb-1">백준</span>
+                                                                <span class="text-end fs-4 card-in-arrow">
+                                                                    <FontAwesomeIcon icon={faCircleArrowUp} className='rot-45deg'/>
                                                                 </span>
                                                             </div>
                                                         </div>
@@ -129,13 +169,23 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <div class="justify-content-center d-flex font-11 fs-1 p-4">포트폴리오 확장을 위해 계속 노력하고 있습니다!</div>
+            <div class="main__action text-center">
+                <div className="main__scroll">
+                    <div class="main__scroll-box">
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M11.9997 13.1716L7.04996     8.22186L5.63574 9.63607L11.9997 16L18.3637 9.63607L16.9495 8.22186L11.9997 13.1716Z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="section" id="intro">
             <div class="bg-white-gray">
                 <div class="container-xxl h-100 pt-7">
                     <div class="s-intro">
                         <div class="s-intro__content row text-black pt-5"> 
-
                                 <Fade bottom delay={500}>
                                     <div>
                                         <div class="fs-0-5 font-11 fw-light2 font-letter-space-sm text-black mb-5 pt-5">MORE AND MORE <span class="fw-bold">CREATIVELY</span></div>
