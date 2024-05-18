11fcea6a06778c24c8156792c966aeef4ab434c2
usfree <tlrmsjtm77@gmail.com>
2024-05-19 02:47:04 +0900

    orbit 0519

--- src/component/Project/Orbit.js
+++ src/component/Project/Orbit.js
@@ -25,7 +25,7 @@ import toast from 'react-hot-toast';
 
 
 function Orbit() {
-    const updatedate = "2024. 05. 17." //최종 업데이트 일자
+    const updatedate = "2024. 05. 19." //최종 업데이트 일자
     const notify = () => toast.error('제작 진행중입니다.', {duration: 4000});
     const notify2 = () => toast.error('제작 진행중인 프로젝트입니다. 깃허브 리포지토리를 아직 제공하지 않습니다.', {duration: 4000});
     useEffect(() => {
