208c2adbc145dc630c02535bd479ff7ad52b93cb
usfree <tlrmsjtm77@naver.com>
2024-06-02 03:33:05 +0900

    메인 섹션1 비디오 블러추가

--- src/component/Home/Home_1.js
+++ src/component/Home/Home_1.js
@@ -75,7 +75,7 @@ function Home_1() {
         <div className="scroll-container_hor" ref={scrollRef}>
 
           <div className="section_hor">
-            <video className="background-video_hor" poster={poster} autoPlay muted loop>
+            <video className="background-video_hor video-filter" poster={poster} autoPlay muted loop>
                 <source src={intro} type="video/mp4" />
             </video>
             <div className="text-container_hor">
@@ -90,7 +90,7 @@ function Home_1() {
           </div>
 
           <div className="section_hor">
-                <video className="background-video_hor" poster={poster2} autoPlay muted loop>
+                <video className="background-video_hor video-filter" poster={poster2} autoPlay muted loop>
                     <source src={intro2} type="video/mp4" />
                 </video>
                 <div className="text-container_hor">
@@ -105,7 +105,7 @@ function Home_1() {
           </div>
 
           <div className="section_hor">
-                <video className="background-video_hor" poster={poster3} autoPlay muted loop>
+                <video className="background-video_hor video-filter" poster={poster3} autoPlay muted loop>
                     <source src={intro3} type="video/mp4" />
                 </video>
                 <div className="text-container_hor">
@@ -120,7 +120,7 @@ function Home_1() {
           </div>
 
           <div className="section_hor">
-                <video className="background-video_hor" poster={poster4} autoPlay muted loop>
+                <video className="background-video_hor video-filter" poster={poster4} autoPlay muted loop>
                     <source src={intro4} type="video/mp4" />
                 </video>
                 <div className="text-container_hor">
@@ -135,7 +135,7 @@ function Home_1() {
           </div>
 
           <div className="section_hor">
-                <video className="background-video_hor" poster={poster1} autoPlay muted loop>
+                <video className="background-video_hor video-filter" poster={poster1} autoPlay muted loop>
                     <source src={intro1} type="video/mp4" />
                 </video>
                 <div className="text-container_hor">
