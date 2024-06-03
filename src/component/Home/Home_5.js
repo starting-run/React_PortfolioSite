48ce9230762db5f297cc80557dceb6109ba8186f
Woohyun Cho <tlrmsjtm77@naver.com>
2024-06-03 13:30:24 +0900

    프록시 테스트

--- src/component/Home/Home_5.js
+++ src/component/Home/Home_5.js
@@ -10,7 +10,7 @@ function Home_5() {
   let currentIndex = 0;
 
   const corsProxies = [
-    'https://proxy.cors.sh/',
+    'https://cors-proxy.org/api/',
     'https://corsproxy.io/?',
     //프록시추가가능
   ];
