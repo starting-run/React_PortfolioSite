bb6da5ee6fc98068796a8e9c2a3a310436bacde9
Woohyun Cho <tlrmsjtm77@naver.com>
2024-06-03 10:20:58 +0900

    프록시서버 변경

--- src/component/Home/Home_5.js
+++ src/component/Home/Home_5.js
@@ -10,7 +10,7 @@ function Home_5() {
   let currentIndex = 0;
 
   const corsProxies = [
-    'https://cors-anywhere.herokuapp.com/',
+    'https://corsproxy.io/?',
     //'https://thingproxy.freeboard.io/fetch/', //localhost용
     //프록시추가가능
   ];
