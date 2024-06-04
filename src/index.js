1fd30313761f965d284092e503614dbb33f1e9e1
Woohyun Cho <tlrmsjtm77@naver.com>
2024-06-04 13:44:24 +0900

    롤백

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {Route, Routes, HashRouter, BrowserRouter} from 'react-router-dom'; 
+import {Route, Routes, HashRouter} from 'react-router-dom'; 
 import ScrollToTop from "./component/Function/ScrollToTop";
 import ScrollIndicator from './component/Function/ScrollIndicator';
 import Home from './component/Home/Home';
@@ -22,7 +22,7 @@ import Footer from './component/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
     {/*BrowserRouter*/}
       <Toaster position='bottom-right'/>
       <App />
@@ -41,7 +41,7 @@ root.render(
       <ScrollIndicator/>
       <Nav />
       <Footer/>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 
