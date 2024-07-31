95f99050fc487e90d46678e58b932be61b035204
Woohyun Cho <46083286+starting-run@users.noreply.github.com>
2024-07-31 17:19:26 +0900

    폰트크기 재수정

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -114,7 +114,7 @@ return (
                 </label>
               </button>
               <div className="collapse navbar-collapse justify-content-center-navbar navbar-left-margin" id="navbarSupportedContent"> {/* navbar-left-margin 가운데 정렬용 */}
-                <ul className="navbar-nav mb-2 mb-lg-0 fs-6">
+                <ul className="navbar-nav mb-2 mb-lg-0 fs-6-5">
                   <NavLink exact='true' to="/profile" className="nav-link navbar-items" title="Profile"><li className="nav-item">프로필</li></NavLink>
                   {/*<NavLink exact='true' to="/link" className="nav-link navbar-items" title="link"><li className="nav-item">링크</li></NavLink>*/}
                   <NavLink exact='true' to="/project" className="nav-link navbar-items" title="Project"><li className="nav-item">프로젝트</li></NavLink>
