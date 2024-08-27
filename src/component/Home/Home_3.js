5c567d8173e69591c359d4376d87a7d3adc76257
Woohyun Cho <tlrmsjtm77@naver.com>
2024-08-28 00:57:18 +0900

    메인 오르빗 영상 컨트롤러 숨김

--- src/component/Home/Home_3.js
+++ src/component/Home/Home_3.js
@@ -69,7 +69,7 @@ function Home_3() {
                             <div className="col w-100 mb-4"> {/*col-lg-12*/}
                                 <div className="card  card-border-0 p-0 rounded-4">
                                     <div className="card-image-sec0">
-                                        <iframe src="https://www.youtube.com/embed/MR-p7XxmcoI?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                        <iframe src="https://www.youtube.com/embed/MR-p7XxmcoI?autoplay=1&mute=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                                     </div>
                                     
                                     {/*
