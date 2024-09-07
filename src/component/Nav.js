f3ead889c08057ea1cb7e74bded01132d08f7486
Woohyun Cho <tlrmsjtm77@naver.com>
2024-09-08 01:54:00 +0900

    네비바 폰트 크기 수정

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -86,7 +86,7 @@ function Nav() {
 
   const navbarClass = currentPath === '/' && scrollPos === 0 ? 'navbar-py2' : 'navbar-py';
   const navbarClass2 = currentPath === '/' && scrollPos === 0 ? 'container-md' : 'container-full';
-  const navbarClass3 = currentPath === '/' && scrollPos === 0 ? 'fs-4' : 'fs-5-5';
+  const navbarClass3 = currentPath === '/' && scrollPos === 0 ? 'fs-5' : 'fs-5-5';
 
   // 드롭다운의 가시성을 관리하는 상태
   const [dropdownVisible, setDropdownVisible] = useState(false);
