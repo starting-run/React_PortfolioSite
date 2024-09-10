e0fc1900ffb179a5d356f0c68c577d95133ee0a1
Woohyun Cho <tlrmsjtm77@naver.com>
2024-09-10 22:35:38 +0900

    navbar 스크롤 애니메이션 제거

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -84,9 +84,9 @@ function Nav() {
     //notify();
   }, []);
 
-  const navbarClass = currentPath === '/' && scrollPos === 0 ? 'navbar-py2' : 'navbar-py';
-  const navbarClass2 = currentPath === '/' && scrollPos === 0 ? 'container-md' : 'container-full';
-  const navbarClass3 = currentPath === '/' && scrollPos === 0 ? 'fs-5' : 'fs-5-5';
+  const navbarClass = currentPath === '/' && scrollPos === 0 ? 'navbar-py' : 'navbar-py'; //왼쪽 navbar-py2
+  const navbarClass2 = currentPath === '/' && scrollPos === 0 ? 'container-full' : 'container-full'; //왼쪽  container-md 
+  const navbarClass3 = currentPath === '/' && scrollPos === 0 ? 'fs-5-5' : 'fs-5-5'; // 왼쪽 fs-5
 
   // 드롭다운의 가시성을 관리하는 상태
   const [dropdownVisible, setDropdownVisible] = useState(false);
