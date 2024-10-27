2206661a031b810d7f2da753683b233a021435eb
Woohyun Cho <tlrmsjtm77@naver.com>
2024-10-28 00:35:59 +0900

    navbar 폰트 굵기, 크기, uppercase로 변경

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -86,7 +86,7 @@ function Nav() {
 
   const navbarClass = currentPath === '/' && scrollPos === 0 ? 'navbar-py' : 'navbar-py'; //왼쪽 navbar-py2
   const navbarClass2 = currentPath === '/' && scrollPos === 0 ? 'container-full' : 'container-full'; //왼쪽  container-md 
-  const navbarClass3 = currentPath === '/' && scrollPos === 0 ? 'fs-5-5' : 'fs-5-5'; // 왼쪽 fs-5
+  const navbarClass3 = currentPath === '/' && scrollPos === 0 ? 'fs-6' : 'fs-6'; // 왼쪽 fs-5
 
   // 드롭다운의 가시성을 관리하는 상태
   const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -115,12 +115,12 @@ function Nav() {
                   </label>
                 </button>
                 <div className="collapse navbar-collapse justify-content-center-navbar navbar-left-margin" id="navbarSupportedContent"> {/* navbar-left-margin 가운데 정렬용 */}
-                  <ul className={`navbar-nav mb-2 mb-lg-0 ${navbarClass3}`}>
-                    <NavLink exact='true' to="/about" className="nav-link navbar-items" title="Profile"><li className="nav-item font-3 fw-semibold">About</li></NavLink>
+                  <ul className={`navbar-nav mb-2 mb-lg-0 text-uppercase ${navbarClass3}`}>
+                    <NavLink exact='true' to="/about" className="nav-link navbar-items" title="Profile"><li className="nav-item font-3 fw-normal">About</li></NavLink>
                     {/*<NavLink exact='true' to="/link" className="nav-link navbar-items" title="link"><li className="nav-item">링크</li></NavLink>*/}
-                    <NavLink exact='true' to="/project" className="nav-link navbar-items" title="Project"><li className="nav-item font-3 fw-semibold">Project</li></NavLink>
-                    <NavLink exact='true' to="/blog" className="nav-link navbar-items" title="blog"><li className="nav-item font-3 fw-semibold">Blog</li></NavLink>
-                    <a className="nav-link navbar-items" href="https://github.com/starting-run" target='_blank' title="Github"><li className="nav-item font-3 fw-semibold">Github <FontAwesomeIcon icon={faArrowUp} className='rot-45deg fs-7' id='github-icon'/></li></a>
+                    <NavLink exact='true' to="/project" className="nav-link navbar-items" title="Project"><li className="nav-item font-3 fw-normal">Project</li></NavLink>
+                    <NavLink exact='true' to="/blog" className="nav-link navbar-items" title="blog"><li className="nav-item font-3 fw-normal">Blog</li></NavLink>
+                    <a className="nav-link navbar-items" href="https://github.com/starting-run" target='_blank' title="Github"><li className="nav-item font-3 fw-normal">Github <FontAwesomeIcon icon={faArrowUp} className='rot-45deg fs-7' id='github-icon'/></li></a>
 
                     <span className="hamburger-copy text-black pt-5 font-letter-space only-mobile-view font-11">Copyright &copy; Woohyun Cho(STARTING)<br/>All rights reserved.</span>
                     <span className="size-repack"></span> {/* animation 부드럽게 처리하기 위함 */}
