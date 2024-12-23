03b0bc5680f3497510bdd9d7efb025f6d1faa9a9
Woohyun Cho <tlrmsjtm77@naver.com>
2024-12-23 19:33:26 +0900

    네비바 폰트 변경경

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -90,11 +90,11 @@ function Nav() {
                 </button>
                 <div className="collapse navbar-collapse justify-content-center-navbar navbar-left-margin navbar-upper" id="navbarSupportedContent"> {/* navbar-left-margin 가운데 정렬용 */}
                   <ul className={`navbar-nav mb-2 mb-lg-0 font-letter-space-lsm ${navbarClass3}`}>
-                    <NavLink exact='true' to="/about" className="nav-link navbar-items" title="Profile"><li className="nav-item font-3 fw-semibold">프로필</li></NavLink>
+                    <NavLink exact='true' to="/about" className="nav-link navbar-items" title="Profile"><li className="nav-item font-11 fw-bold">프로필</li></NavLink>
                     {/*<NavLink exact='true' to="/link" className="nav-link navbar-items" title="link"><li className="nav-item">링크</li></NavLink>*/}
-                    <NavLink exact='true' to="/project" className="nav-link navbar-items" title="Project"><li className="nav-item font-3 fw-semibold">프로젝트</li></NavLink>
-                    <NavLink exact='true' to="/blog" className="nav-link navbar-items" title="blog"><li className="nav-item font-3 fw-semibold">블로그</li></NavLink>
-                    <a className="nav-link navbar-items" href="https://github.com/starting-run" target='_blank' title="Github"><li className="nav-item font-3 fw-semibold">깃허브 <FontAwesomeIcon icon={faArrowUp} className='rot-45deg fs-7' id='github-icon'/></li></a>
+                    <NavLink exact='true' to="/project" className="nav-link navbar-items" title="Project"><li className="nav-item font-11 fw-bold">프로젝트</li></NavLink>
+                    <NavLink exact='true' to="/blog" className="nav-link navbar-items" title="blog"><li className="nav-item font-11 fw-bold">블로그</li></NavLink>
+                    <a className="nav-link navbar-items" href="https://github.com/starting-run" target='_blank' title="Github"><li className="nav-item font-11 fw-bold">깃허브 <FontAwesomeIcon icon={faArrowUp} className='rot-45deg fs-7' id='github-icon'/></li></a>
 
                     {/* <span className="hamburger-copy text-black pt-5 font-letter-space only-mobile-view font-11">Copyright &copy; Woohyun Cho(STARTING)<br/>All rights reserved.</span> */}
                     <span className="size-repack"></span> {/* animation 부드럽게 처리하기 위함 */}
